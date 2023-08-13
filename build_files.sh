a81b8b617ab2f8913954d57726e1d26672931baf
dhruv <dhruv20345@gmail.com>
2023-08-13 16:35:37 +0530

    t

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,9 @@
 echo "BUILD START"
-
-# Create a virtual environment
-# python3.9 -m venv venv
-
-# Activate the virtual environment (on Windows)
-# source venv/Scripts/activate  # Use backslashes on Windows
-
 # Install dependencies
 python3.9 -m pip install -r requirements.txt
 
 # Collect static files
-python3.9 manage.py collectstatic
+python3.9 manage.py collectstatic --noinput --clear
 
-# Deactivate the virtual environment
-deactivate  # Deactivate the virtual environment
 
 echo "BUILD END"
