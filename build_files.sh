609fa455c5a32408292b7f33f7e4952a2d77f182
dhruv <dhruv20345@gmail.com>
2023-08-13 10:52:39 +0530

    test 2

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 
 echo "BUID START"
 python3.9 -m venv venv
-venv\Scripts\activate
+./venv\Scripts\activate
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic 
 echo "BUID END" 
