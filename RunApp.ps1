f295d377d7d86d3edf6a4f02b1633c97c2e991b6
dhruv <dhruv20345@gmail.com>
2023-08-26 23:12:45 +0530

    tst 7

---
+++ RunApp.ps1
@@ -0,0 +1,23 @@
+# Set the project directory and virtual environment name
+$venvName = "venv"
+
+# Check if the virtual environment folder exists
+if (-not Test-Path ($venvName)) {
+    # Create virtual environment if it doesn't exist
+    Write-Host "Creating virtual environment..."
+    python -m venv $venvName
+}
+
+Write-Host "Installing dependencies..."
+python -m pip install -r "requirements.txt"
+
+try {
+    & venv\Scripts\Activate.ps1
+    # Run your Django app (replace "myproject" with your actual project name)
+    Write-Host "Running Django app..."
+    python manage.py runserver
+
+} catch {
+    Write-Host "An error occurred: $($_.Exception.Message)"
+    # Install dependencies from requirements.txt
+}
--- myserver/urls.py
+++ myserver/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from myserver.home import *
 
 urlpatterns = [
-    # path('user/', include('myserver.User.routes')),
+    path('user/', include('myserver.User.routes')),
     # path('entry/', include('myserver.Entries.routes')),
     # path('group/', include('myserver.Group.routes')),
 ]
