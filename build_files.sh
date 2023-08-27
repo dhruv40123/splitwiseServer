89ef0b1adf46041e480fe04d69c92ea74d452fe5
dhruv <dhruv20345@gmail.com>
2023-08-27 08:46:42 +0530

    zzz

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,9 @@
-# Create and activate a virtual environment
-# python3.9 -m venv venv
-# source venv/bin/activate
+
 
 # Install requirements and collect static files
 echo "BUILD START"
 pip install -r requirements.txt
-python manage.py collectstatic --clear
+python3.9 manage.py collectstatic --clear
 echo "BUILD END"
 
 # Deactivate the virtual environment
